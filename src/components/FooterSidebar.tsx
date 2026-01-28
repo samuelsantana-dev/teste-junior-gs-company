c912790093b91ade1be839c4bdb5e2968e314f7a
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2026-01-28 16:33:12 -0300

    refactor: Refactor components and new implementation

---
+++ src/components/FooterSidebar.tsx
@@ -0,0 +1,7 @@
+export function FooterSidebar(){
+    return (
+         <div className="p-3 border-t border-gray-100 bg-gray-50 text-xs text-center text-gray-400">
+        GS Company CRM v1.0
+      </div>
+    )
+}
